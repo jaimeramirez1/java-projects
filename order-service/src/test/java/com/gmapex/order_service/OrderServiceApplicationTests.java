faeb4845d97ed09ce330a49ce89b661f9cf6ee61
jaimeramirez1 <jaime.ramirez1010@gmail.com>
2025-10-24 16:11:49 -0500

    Initial project structure

---
+++ order-service/src/test/java/com/gmapex/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmapex.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
